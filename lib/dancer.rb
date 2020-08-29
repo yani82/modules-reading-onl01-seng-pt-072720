7a8f6fcc14af274658cfffc6abdd402b93de647b
Yani Asari <yani.iman1@gmail.com>
2020-08-29 17:21:37 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,12 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
-  extend MetaDancing
-  include Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
