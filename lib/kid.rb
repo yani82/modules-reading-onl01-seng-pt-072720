d74ec55a78d39fedc40c52132aa132e0b4300630
Yani Asari <yani.iman1@gmail.com>
2020-08-29 17:25:39 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,12 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+   extend MetaDancing
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
